475d41d48b49329573e2785dff6c47439618df2f
djorges <jorgesdiego@hotmail.com>
2026-09-04 23:46:43 -0300

    chore: configura proyecto backend

---
+++ Backend/ServiceFlow/src/test/java/com/nocountry/serviceflow/ServiceFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.serviceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
